102610f5f4a8e48fcb5c2c82e13041c477705d7f
kalin <kalin.stanchev86@gmail.com>
2022-09-04 10:33:29 +0300

    ad hist plot visualization

---
+++ diabetes_prediction_keras/Visualizator.py
@@ -0,0 +1,25 @@
+import seaborn as sns
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+class Visualizator:
+
+    def __init__(self, df):
+        self.df = df
+        self.sns = sns
+
+    def show_confusion_matrix(self, c_matrix):
+        ax = self.sns.heatmap(c_matrix, annot=True,
+                              xticklabels=['No Diabetes', 'Diabetes'],
+                              yticklabels=['No Diabetes', 'Diabetes'],
+                              cbar=False, cmap='Blues')
+        ax.set_xlabel("Prediction")
+        ax.set_ylabel("Actual")
+        plt.show()
+
+    def show_hist_plot_dataset(self, diabetes_dataset, x):
+        self.sns.histplot(x=x, data=diabetes_dataset, color='red')
+        plt.show()
+
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from diabetes_prediction_custom.App import App as AppCustom
 from diabetes_prediction_keras.App import App as AppKeras
+from diabetes_prediction_keras.Visualizator import Visualizator
+import pandas as pd
 
 CSV_FILE_PATH = 'dataset/diabetes.csv'
 
@@ -16,19 +18,27 @@ def diabetes_prediction_custom():
 
 
 def diabetes_prediction_keras():
-
-    app = AppKeras(CSV_FILE_PATH)
+    df = pd.read_csv('dataset/diabetes.csv')
+    visualizator = Visualizator(df)
+    app = AppKeras(df)
     app.replace_zero_column()
     app.fill_null_columns()
     app.standard_data()
     app.separate_data()
     app.build()
+    app.test_accuracy()
 
-    patient_data = app.get_patient_data(input_data)
+    patient_results = [9, 102, 76, 37, 0, 32.9, 0.665, 46]
+    patient_data = app.get_patient_data(patient_results)
     app.predict(patient_data)
 
+    c_matrix = app.get_confusion_matrix(patient_data)
+    # visualizator.show_confusion_matrix(c_matrix)
+    visualizator.show_hist_plot_dataset(df, x='Glucose')
 
 # diabetes_prediction_custom()
+
+
 diabetes_prediction_keras()
 
 
