4e446d964397f9a4219b3867dc1d5eb7c018779e
kalin <kalin.stanchev86@gmail.com>
2022-09-04 10:20:14 +0300

    Add diabetes prediction with neuron network using keras.

--- diabetes_prediction_keras/App.py
+++ diabetes_prediction_keras/App.py
@@ -4,13 +4,14 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from keras import Sequential
 from keras.layers import Dense
+from sklearn.metrics import confusion_matrix
 
 
 class App:
 
-    def __init__(self, csv_path):
+    def __init__(self, df):
         self.model = None
-        self.df = pd.read_csv(csv_path)
+        self.df = df
         self.df_scaled = None
         self.x = None
         self.y = None
@@ -22,7 +23,6 @@ class App:
         self.y_val = None
 
     def replace_zero_column(self):
-        # print(self.df.colums)
         self.df['Glucose'] = self.df['Glucose'].replace(0, np.nan)
         self.df['BloodPressure'] = self.df['BloodPressure'].replace(0, np.nan)
         self.df['Pregnancies'] = self.df['Pregnancies'].replace(0, np.nan)
@@ -40,7 +40,8 @@ class App:
         self.df['SkinThickness'] = self.df['SkinThickness'].fillna(self.df['SkinThickness'].mean())
         self.df['Insulin'] = self.df['Insulin'].fillna(self.df['Insulin'].mean())
         self.df['BMI'] = self.df['BMI'].fillna(self.df['BMI'].mean())
-        self.df['DiabetesPedigreeFunction'] = self.df['DiabetesPedigreeFunction'].fillna(self.df['DiabetesPedigreeFunction'].mean())
+        self.df['DiabetesPedigreeFunction'] = self.df['DiabetesPedigreeFunction'].fillna(
+            self.df['DiabetesPedigreeFunction'].mean())
         self.df['Age'] = self.df['Age'].fillna(self.df['Age'].mean())
 
     def standard_data(self):
@@ -59,28 +60,48 @@ class App:
         self.model = Sequential()
         self.model.add(Dense(32, activation='relu', input_dim=8))
         self.model.add(Dense(16, activation='relu'))
-        self.model.add(Dense(1, activation='relu'))
+        self.model.add(Dense(1, activation='sigmoid'))
         self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
         self.model.fit(self.x_train, self.y_train, epochs=200)
 
     def test_accuracy(self):
-        scores = self.model.evaliate(self.x_train, self.y_train)
+        scores = self.model.evaluate(self.x_train, self.y_train)
         print("Training accuracy: %.2f%%\n" % scores[1] * 100)
 
-        scores = self.model.evaliate(self.x_test, self.y_test)
+        scores = self.model.evaluate(self.x_test, self.y_test)
         print("Testing accuracy: %.2f%%\n" % scores[1] * 100)
 
     def predict(self, patient_data):
         y_test_predict = self.model.predict(patient_data)
-        if y_test_predict[0][0] > 50:
+        # classes_x = (self.model.predict(self.x_test) > 0.5).astype("int32")
+        classes_x = (self.model.predict(patient_data) > 0.5).astype("int32")
+        # print(classes_x)
+        if classes_x[0][0] == 1:
             print('Patient is not diabetic')
         else:
             print('Patient is diabetic')
 
+    def get_prediction(self, data):
+
+        return self.model.predict(self.x_test)
+
     def get_patient_data(self, patient_results):
-        df = pd.DataFrame(
+        self.df = pd.DataFrame(
             [patient_results],
             columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI',
                      'DiabetesPedigreeFunction', 'Age'])
 
-        return df
+        return self.df
+
+    def get_x_test(self):
+        return self.x_test
+
+    def get_confusion_matrix(self, y_prediction):
+        # y_test_predict = self.get_prediction(y_prediction)
+        # y_test_predict = self.model.predict(self.x_test)
+        # classes_x = np.argmax(y_test_predict, axis=1)
+        classes_x = (self.model.predict(self.x_test) > 0.5).astype("int32")
+        c_matrix = confusion_matrix(self.y_test, classes_x)
+
+        return c_matrix
+
